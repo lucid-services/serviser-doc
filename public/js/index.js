4dd935cf25eb9467b53c160a9c082121910eb4e9
fogine <7884288+fogine@users.noreply.github.com>
2017-03-02 20:20:49 +0100

    bugfixes, support for multiple doc spec versions

--- public/js/index.js
+++ public/js/index.js
@@ -1,58 +1,68 @@
 $(function () {
-    $.get( "/specs", function( data ) {
-        console.log(typeof data);
-        console.log(data);
-    });
-
     var supportedSubmitMethods = [];
 
     if (window.docSettings.tryItOut) {
         supportedSubmitMethods = ['get', 'post', 'put', 'delete', 'patch'];
     }
 
-
     // Pre load translate...
     if(window.SwaggerTranslator) {
         window.SwaggerTranslator.translate();
     }
-    window.swaggerUi = new SwaggerUi({
-        url: window.docSettings.url,
-        jsonEditor: false,
-        dom_id: "swagger-ui-container",
-        supportedSubmitMethods: supportedSubmitMethods,
-        onComplete: function(swaggerApi, swaggerUi){
-            if(typeof initOAuth == "function") {
-                initOAuth({
-                    clientId: "your-client-id",
-                    clientSecret: "your-client-secret-if-required",
-                    realm: "your-realms",
-                    appName: "your-app-name",
-                    scopeSeparator: ",",
-                    additionalQueryStringParams: {}
+
+    $.get( "/specs", function( specs ) {
+        var versions = Object.keys(specs);
+
+        $('select').selectBox();
+        $('select').selectBox("options", versions);
+
+        $('select').selectBox().change(function () {
+            var spec = specs[versions[$(this).val()]];
+            window.swaggerUi.setOption("spec", spec);
+            window.swaggerUi.updateSwaggerUi({spec: spec});
+        });
+
+        window.swaggerUi = new SwaggerUi({
+            url: '',
+            spec: specs[versions[0]],
+            jsonEditor: false,
+            validatorUrl: null,
+            dom_id: "swagger-ui-container",
+            supportedSubmitMethods: supportedSubmitMethods,
+            onComplete: function(swaggerApi, swaggerUi){
+                if(typeof initOAuth == "function") {
+                    initOAuth({
+                        clientId: "your-client-id",
+                        clientSecret: "your-client-secret-if-required",
+                        realm: "your-realms",
+                        appName: "your-app-name",
+                        scopeSeparator: ",",
+                        additionalQueryStringParams: {}
+                    });
+                }
+
+                if(window.SwaggerTranslator) {
+                    window.SwaggerTranslator.translate();
+                }
+
+                $('pre code').each(function(i, e) {
+                    hljs.highlightBlock(e)
                 });
-            }
 
-            if(window.SwaggerTranslator) {
-                window.SwaggerTranslator.translate();
-            }
+                addApiKeyAuthorization();
+            },
+            onFailure: function(data) {
+                console.log("Unable to Load SwaggerUI");
+            },
+            docExpansion: "none",
+            apisSorter: "alpha",
+            defaultModelRendering: 'schema',
+            showRequestHeaders: false
+        });
 
-            $('pre code').each(function(i, e) {
-                hljs.highlightBlock(e)
-            });
-
-            addApiKeyAuthorization();
-        },
-        onFailure: function(data) {
-            console.log("Unable to Load SwaggerUI");
-        },
-        docExpansion: "none",
-        apisSorter: "alpha",
-        defaultModelRendering: 'schema',
-        showRequestHeaders: false
+        $('#input_apiKey').change(addApiKeyAuthorization);
+        window.swaggerUi.load();
     });
-
-    $('#input_apiKey').change(addApiKeyAuthorization);
-    window.swaggerUi.load();
 
     function addApiKeyAuthorization(){
         //var key = encodeURIComponent($('#input_apiKey')[0].value);
