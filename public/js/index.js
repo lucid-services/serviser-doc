f8bd18134599641eb1a60edeff91187d5672f078
fogine <7884288+fogine@users.noreply.github.com>
2018-05-02 15:02:39 +0200

    front-end features

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,70 @@
+function filterBox() {
+    var input = $('#filterBox').attr('value');
+    var re = new RegExp(input);
+
+    Object.keys(window.swaggerUi.api.apis).map(function(group) {
+        window.Docs.expandEndpointListForResource(group);
+    });
+
+
+    if (typeof filterBox.prevInput === 'string') {
+        if (filterBox.prevInput === input) {
+            return;
+        }
+    }
+
+    var operations = window.swaggerUi.api.apisArray.reduce(function(out, api) {
+        api.operationsArray.forEach(function(op) {
+            out.push(op);
+        });
+
+        return out;
+    }, []);
+
+    var toBeHiddenOperationIds = [];
+    var toBeDisplayedOperationIds = [];
+
+    operations.forEach(function(op) {
+        var url = op.host + op.basePath + op.path;
+        var ids = [];
+        (op.operation.tags || []).forEach(function(tag) {
+            ids.push('#' + tag + '_' + op.nickname);
+        });
+
+        if (   !url.match(re)
+            && !op.operation.summary.match(re)
+            && !((op.vendorExtensions['x-sdkMethodName'] || '').match(re))
+            && !op.operation.description.match(re)
+        ) {
+            toBeHiddenOperationIds = toBeHiddenOperationIds.concat(ids);
+        } else {
+            toBeDisplayedOperationIds = toBeDisplayedOperationIds.concat(ids);
+        }
+    });
+
+    $(toBeHiddenOperationIds.join(', ')).css('display', 'none');
+    $(toBeDisplayedOperationIds.join(', ')).css('display', 'block');
+}
+
+(function($) {
+    $.QueryString = (function(paramsArray) {
+        let params = {};
+
+        for (let i = 0; i < paramsArray.length; ++i)
+        {
+            let param = paramsArray[i]
+                .split('=', 2);
+
+            if (param.length !== 2)
+                continue;
+
+            params[param[0]] = decodeURIComponent(param[1].replace(/\+/g, " "));
+        }
+
+        return params;
+    })(window.location.search.substr(1).split('&'))
+})(jQuery);
+
 $(function () {
     var supportedSubmitMethods = [];
 
@@ -12,19 +79,23 @@ $(function () {
 
     $.get( "/specs", function( specs ) {
         var versions = Object.keys(specs);
+        var apiVersion = versions.indexOf($.QueryString.api_version || versions[0]);
 
         $('select').selectBox();
         $('select').selectBox("options", versions);
+        $('select').selectBox('value', apiVersion);
 
         $('select').selectBox().change(function () {
             var spec = specs[versions[$(this).val()]];
             window.swaggerUi.setOption("spec", spec);
             window.swaggerUi.updateSwaggerUi({spec: spec});
+            $.QueryString.api_version = versions[$(this).val()];
+            history.pushState({}, '', '?' + $.param($.QueryString));
         });
 
         window.swaggerUi = new SwaggerUi({
             url: '',
-            spec: specs[versions[0]],
+            spec: specs[versions[apiVersion]],
             jsonEditor: false,
             showOperationIds: true,
             validatorUrl: null,
